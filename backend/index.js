6af7d6430bc45c4108556d7ed15e06ffe57b6776
Sakshi Dargu <124140455+SakshiDargu@users.noreply.github.com>
2024-10-26 19:40:56 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,9 @@ var bodyParser = require('body-parser');
 const { Pool } = require('pg');
 
 const PORT =  5000;
-var corsoption = {
-    origin: 'https://project-gto8.onrender.com',
-  };
 
   const app = express();
-  app.use(cors(corsoption));
+  app.use(cors());
   app.use(express.json())
 
 const pool = new Pool({
@@ -25,9 +22,23 @@ const createTableQuery = `
     name TEXT NOT NULL,
     description TEXT,
     price FLOAT,
-    quantity INTEGER
+    quantity INTEGER,
+    image TEXT
   );
 `;
+
+async function dropProductsTable() {
+  const dropQuery = `DROP TABLE IF EXISTS products;`; // SQL query to drop the products table
+
+  try {
+    await pool.query(dropQuery);
+    console.log('Table "products" dropped successfully.');
+  } catch (error) {
+    console.error('Error dropping table:', error);
+  }
+}
+
+// dropProductsTable();
 
 async function createTable() {
   try {
@@ -48,6 +59,7 @@ app.get('/api/products', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM products');
     res.json(result.rows);
+    console.log(result.rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -55,11 +67,12 @@ app.get('/api/products', async (req, res) => {
 
 // POST operation
 app.post('/api/products', async (req, res) => {
-  const { name, description, price, quantity } = req.body;
+  const { name, description, price, quantity, image } = req.body;
+  console.log(name, description, price, quantity, image)
   try {
     const result = await pool.query(
-      'INSERT INTO products (name, description, price, quantity) VALUES ($1, $2, $3, $4) RETURNING *',
-      [name, description, price, quantity]
+      'INSERT INTO products (name, description, price, quantity, image) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [name, description, price, quantity, image]
     );
     res.json(result.rows[0]);
   } catch (error) {
@@ -70,11 +83,12 @@ app.post('/api/products', async (req, res) => {
 // PUT operation
 app.put('/api/products/:id', async (req, res) => {
   const { id } = req.params;
-  const { name, description, price, quantity } = req.body;
+  const { name, description, price, quantity, image } = req.body;
+
   try {
     const result = await pool.query(
-      'UPDATE products SET name = $1, description = $2, price = $3, quantity = $4 WHERE id = $5 RETURNING *',
-      [name, description, price, quantity, id]
+      'UPDATE products SET name = $1, description = $2, price = $3, quantity = $4, image = $6 WHERE id = $5 RETURNING *',
+      [name, description, price, quantity, id, image]
     );
     res.json(result.rows[0]);
   } catch (error) {
